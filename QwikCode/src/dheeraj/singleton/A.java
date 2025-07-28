d5facbebb3fa62022980e6ea2ca005db59015cf0
Dheeraj Patel <dheeraj2001@gmial.com>
2025-07-28 12:18:16 +0530

    this is Singletgon Updated Code

--- QwikCode/src/dheeraj/singleton/A.java
+++ QwikCode/src/dheeraj/singleton/A.java
@@ -1,29 +1,86 @@
+/*
+Singleton Class:
+-We design the class in such that only one object of that class at most can be created.
+How it works now?
+When multiple threads call getInstance() at the same time,
+only one thread can enter the method at a time because of the synchronized lock.
+Therefore, only one object will be created, no matter how many threads run
+ */
 package dheeraj.singleton;
 
 public class A {
 
-	private static A a1=null;
+	private static A a1 = null;
+
 	private A() {
 
-
 	}
-	public static A getInstance() {
 
-		if(a1==null) {
-			a1= new A();
+	public static   A getInstance() {
+
+		if (a1 == null) {
+			System.out.println(" creating objects.....");
+			a1 = new A();
+
 		}
 		return a1;
 
 	}
-	public static void main(String[]args) {
-		A a1= A.getInstance();
-		A a2=A.getInstance();
-		System.out.println(a1);
-		System.out.println(a2);
 
-		System.out.println(a1==a2);
-
+	public static void main(String[] args) {
+//		A a1= A.getInstance();
+//		A a2=A.getInstance();
+//		System.out.println(a1);
+//		System.out.println(a2);
+//
+//		System.out.println(a1==a2);
 
 	}
 
 }
+
+class Test extends Thread {
+
+	public void run() {
+		System.out.println(" thread......");
+		A a1 = A.getInstance();
+	}
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new Test());
+		t1.start();
+
+		Thread t2 = new Thread(new Test());
+		t2.start();
+
+		Thread t3 = new Thread(new Test());
+		t3.start();
+
+		Thread t4 = new Thread(new Test());
+		t4.start();
+
+		Thread t5 = new Thread(new Test());
+		t5.start();
+
+	}
+
+}
+
+
+/* 
+OUTPUT:
+thread......
+thread......
+thread......
+thread......
+thread......
+creating objects.....
+OR
+thread......
+creating objects.....
+thread......
+thread......
+thread......
+thread......
+
+*/
